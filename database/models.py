f23137b73f4758a159d192c626d8373cf4a73ae0
IVAN MERKULOV <ivan8597@yandex.ru>
2026-07-13 09:22:17 +0300

    Update models.py

--- database/models.py
+++ database/models.py
@@ -1,43 +1,20 @@
 from sqlalchemy.orm import DeclarativeBase
-from sqlalchemy import Column, Integer, String, Float
-
+from sqlalchemy import Column, Integer, String, Float, DateTime
+from datetime import datetime
 
 class Base(DeclarativeBase):
     pass
 
-
-
 class Sale(Base):
-
     __tablename__ = "sales"
 
-
-    id = Column(
-        Integer,
-        primary_key=True
-    )
-
-
-    product_name = Column(
-        String
-    )
-
-
-    manager_name = Column(
-        String
-    )
-
-
-    amount = Column(
-        Float
-    )
-
-
-    profit = Column(
-        Float
-    )
-
-
-    margin = Column(
-        Float
-    )
+    id = Column(Integer, primary_key=True, index=True)
+    date = Column(DateTime, default=datetime.utcnow)
+    product_id = Column(Integer)
+    manager_id = Column(Integer)
+    amount = Column(Float)
+    product_name = Column(String)
+    manager_name = Column(String)
+    profit = Column(Float)
+    margin = Column(Float)
+    region = Column(String)
